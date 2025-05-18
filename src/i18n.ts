72d68183705c497f516c616ffe624b9f4dddbe7f
gjonika <gjonika@yahoo.com>
2025-05-18 19:08:45 +0300

    loc

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
 import en from './locales/en.json';
-
+resources: {
+  en: { translation: en }
+}
 i18n
   .use(initReactI18next)
   .init({
